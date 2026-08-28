34a6f47dfdf22a7ae75420711bbfe35e2de6e8f2
Abhinav Prasad Singh <a91106283@gmail.com>
2026-08-28 22:20:18 +0530

    Add Create Strings program

---
+++ CreateStrings.java
@@ -0,0 +1,15 @@
+public class CreateStrings {
+
+    public static void createString() {
+        String firstName = "Abhinav";
+        String lastName = "Singh";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+
+    public static void main(String[] args) {
+        createString();
+    }
+}
