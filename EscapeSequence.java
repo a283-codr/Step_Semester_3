c2e5d8f72d5cdf1e0b87fa1cf33fdf6a0eb1a04f
Abhinav Prasad Singh <a91106283@gmail.com>
2026-08-28 22:28:19 +0530

    Add Escape Sequence program

---
+++ EscapeSequence.java
@@ -0,0 +1,11 @@
+public class EscapeSequence {
+
+    public static void showEscapeSequence() {
+        String message = "He said, \"Java is easy to learn.\"";
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        showEscapeSequence();
+    }
+}
