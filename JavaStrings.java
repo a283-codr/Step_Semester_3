f4d70cdef645aff9341db6b40a67457bed616d23
Abhinav Prasad Singh <a91106283@gmail.com>
2026-08-28 22:08:33 +0530

    Add Java Strings program

---
+++ JavaStrings.java
@@ -0,0 +1,11 @@
+public class JavaStrings {
+
+    public static void displayString() {
+        String message = "Hello, Java!";
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        displayString();
+    }
+}
